ac7a327411d11f4ab8f8e3f90f54493c86397d2e
Balazs Ferencz <64424862+Pici1985@users.noreply.github.com>
2022-07-24 18:49:19 +0100

    Add project files.

---
+++ TodoApi/Models/Todoitem.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApi.Models
+{
+    public class Todoitem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; } 
+    }
+}
+
+// Add nuget package is the next step
