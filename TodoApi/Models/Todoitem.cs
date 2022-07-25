baf6fa026e9a928ef1fac68fe0a69f76c56b054b
Balazs <balazs@frog.net>
2022-07-25 10:58:53 +0100

    finished with secret

--- TodoApi/Models/Todoitem.cs
+++ TodoApi/Models/Todoitem.cs
@@ -5,7 +5,18 @@
         public long Id { get; set; }
         public string? Name { get; set; }
         public bool IsComplete { get; set; } 
+        public string? Secret { get; set; }
+        public string? AnotherSecret { get; set; }
+
     }
+
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+    
 }
 
 // Add nuget package is the next step
--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -10,7 +10,8 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<TodoContext>(opt =>
     opt.UseInMemoryDatabase("TodoList"));
 
-// builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -18,8 +19,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    // app.UseSwagger();
-    // app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
